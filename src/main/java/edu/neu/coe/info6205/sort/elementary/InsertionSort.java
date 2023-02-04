7e74c4e73c94bfd7f8ae5a7e7f50024781b44bba
Aashay Pawar <pawar.aa@northeastern.edu>
2023-02-04 17:05:08 -0500

    Asg3

--- src/main/java/edu/neu/coe/info6205/sort/elementary/InsertionSort.java
+++ src/main/java/edu/neu/coe/info6205/sort/elementary/InsertionSort.java
@@ -6,7 +6,13 @@ package edu.neu.coe.info6205.sort.elementary;
 import edu.neu.coe.info6205.sort.BaseHelper;
 import edu.neu.coe.info6205.sort.Helper;
 import edu.neu.coe.info6205.sort.SortWithHelper;
+import edu.neu.coe.info6205.util.Benchmark;
+import edu.neu.coe.info6205.util.Benchmark_Timer;
 import edu.neu.coe.info6205.util.Config;
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class InsertionSort<X extends Comparable<X>> extends SortWithHelper<X> {
 
@@ -59,6 +65,10 @@ public class InsertionSort<X extends Comparable<X>> extends SortWithHelper<X> {
         final Helper<X> helper = getHelper();
 
         // FIXME
+        for(int i=from;i<to;i++){
+            int k = i;
+            while(k>0 && helper.swapStableConditional(xs,k--));
+        }
         // END 
     }
 
@@ -66,5 +76,58 @@ public class InsertionSort<X extends Comparable<X>> extends SortWithHelper<X> {
 
     public static <T extends Comparable<T>> void sort(T[] ts) {
         new InsertionSort<T>().mutatingSort(ts);
+    }
+
+    public static void main(String[] args){
+        InsertionSort insertionSort = new InsertionSort();
+        Random random = new Random();
+
+        ArrayList<Integer> array_random = new ArrayList<>();
+        ArrayList<Integer> array_ordered = new ArrayList<>();
+        ArrayList<Integer> array_reverse = new ArrayList<>();
+        ArrayList<Integer> array_partially_ordered = new ArrayList<>();
+
+        for(int i=2000;i<35000;i=i*2){
+
+            for(int j=0;j<i;j++) array_random.add(random.nextInt(i));
+            for(int j=0;j<i;j++) array_ordered.add(j+1);
+            for(int j=0;j<i;j++) array_reverse.add(j-1);
+            for(int j=0;j<i;j++) {
+                if(j>i/2) array_partially_ordered.add(random.nextInt(i));
+                else array_partially_ordered.add(j);
+            }
+
+            Integer[] m_array_random = array_random.toArray(new Integer[0]);
+            Integer[] m_array_ordered = array_ordered.toArray(new Integer[0]);
+            Integer[] m_array_reverse = array_reverse.toArray(new Integer[0]);
+            Integer[] m_array_partially_ordered = array_partially_ordered.toArray(new Integer[0]);
+
+            Benchmark<Boolean>benchmarkRandom = new Benchmark_Timer<>("randomSort",b->{
+                insertionSort.sort(m_array_random.clone(), 0, m_array_random.length);
+            });
+            double resultRandom = benchmarkRandom.run(true, 10);
+
+            Benchmark<Boolean>benchmarkOrdered = new Benchmark_Timer<>("orderedSort",b->{
+                insertionSort.sort(m_array_ordered.clone(), 0, m_array_ordered.length);
+            });
+            double resultOrdered = benchmarkOrdered.run(true, 10);
+
+            Benchmark<Boolean>benchmarkReverse = new Benchmark_Timer<>("reverseSort",b->{
+                insertionSort.sort(m_array_reverse.clone(), 0, m_array_reverse.length);
+            });
+            double resultReverse = benchmarkReverse.run(true, 10);
+
+            Benchmark<Boolean>benchmarkPartiallyOrdered = new Benchmark_Timer<>("partiallyOrderedSort",b->{
+                insertionSort.sort(m_array_partially_ordered.clone(), 0, m_array_partially_ordered.length);
+            });
+            double resultPartiallyOrdered = benchmarkPartiallyOrdered.run(true, 10);
+
+            System.out.println("When array size is: " + i);
+            System.out.println("Random: " + resultRandom);
+            System.out.println("Ordered: " + resultOrdered);
+            System.out.println("Reverse: " + resultReverse);
+            System.out.println("Partially Ordered: " + resultPartiallyOrdered + "\n\n");
+        }
+
     }
 }
--- src/main/java/edu/neu/coe/info6205/util/Timer.java
+++ src/main/java/edu/neu/coe/info6205/util/Timer.java
@@ -57,7 +57,24 @@ public class Timer {
     public <T, U> double repeat(int n, Supplier<T> supplier, Function<T, U> function, UnaryOperator<T> preFunction, Consumer<U> postFunction) {
         logger.trace("repeat: with " + n + " runs");
         // FIXME: note that the timer is running when this method is called and should still be running when it returns. by replacing the following code
-         return 0;
+        logger.trace( "-> " + n + " runs");
+        for(int i=0;i<n;i++){
+            T m = supplier.get();
+            if(preFunction != null){
+                pause();
+                m = preFunction.apply(supplier.get());
+                resume();
+            }
+            U k = function.apply(m);
+            if(postFunction != null){
+                pause();
+                postFunction.accept(k);
+                resume();
+            }
+            lap();
+        }
+        pause();
+        return meanLapTime();
         // END 
     }
 
@@ -177,7 +194,7 @@ public class Timer {
      */
     private static long getClock() {
         // FIXME by replacing the following code
-         return 0;
+         return System.nanoTime();
         // END 
     }
 
@@ -190,7 +207,7 @@ public class Timer {
      */
     private static double toMillisecs(long ticks) {
         // FIXME by replacing the following code
-         return 0;
+         return ticks*Math.pow(10,-6);
         // END 
     }
 
